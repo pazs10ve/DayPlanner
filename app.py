987006dd08090ec858385d456439b5d54001bfbc
Mohit Sharma <sharmamohit85627@gmail.com>
2025-07-01 13:13:44 +0530

    deploying backend

--- app.py
+++ app.py
@@ -44,7 +44,3 @@ def get_day_plan(city: str):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
